45a51898323b3c37b3eb98d9ffc83fb35d1e9d23
ADMIN WIN10@WIN-GNKUB6IDL9R <pustezora@gmail.com>
2023-02-21 11:22:18 +0100

    redux_persist_add

--- project/src/store/index.js
+++ project/src/store/index.js
@@ -1,20 +1,36 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import { categoriesReducer } from './reducers/categories';
 import thunk from 'redux-thunk'
 import { categoryProductsReducer } from './reducers/categoryProducts';
 import { allProductsReducer } from './reducers/allProducts';
 import { productReducer } from './reducers/product';
 import { cartReducer } from './reducers/cart';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 
 
 
 const rootReducer = combineReducers({
+    
     categories: categoriesReducer,
     categoryProducts: categoryProductsReducer,
     allProducts: allProductsReducer,
     product: productReducer,
     cart: cartReducer
+    
 });
 
 
-export const store = createStore(rootReducer, applyMiddleware(thunk));
+const persistConfig = {
+    key: 'root',
+    storage: storage,
+   };
+
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(persistedReducer, compose(applyMiddleware(thunk)));
+//export const store = createStore(rootReducer, applyMiddleware(thunk));
+export const persistor = persistStore(store);
+
